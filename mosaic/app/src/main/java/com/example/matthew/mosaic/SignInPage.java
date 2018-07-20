7723c7c39502a108071121a2a60ab11f3c07888b
Matthew Skay <skaymj@gmail.com>
2018-07-20 16:48:40 -0400

    Button to next page

--- mosaic/app/src/main/java/com/example/matthew/mosaic/SignInPage.java
+++ mosaic/app/src/main/java/com/example/matthew/mosaic/SignInPage.java
@@ -2,8 +2,10 @@ package com.example.matthew.mosaic;
 
 import com.example.matthew.mosaic.database.UserDB;
 import android.arch.persistence.room.Room;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInPage extends AppCompatActivity {
     private UserDB database;
@@ -15,5 +17,10 @@ public class SignInPage extends AppCompatActivity {
 
         // create database
         database = Room.databaseBuilder(getApplicationContext(), UserDB.class, "USERDB").build();
+    }
+
+    public void signIn(View view) {
+        Intent intent = new Intent(SignInPage.this, BaseImageActivity.class);
+        startActivity(intent);
     }
 }
