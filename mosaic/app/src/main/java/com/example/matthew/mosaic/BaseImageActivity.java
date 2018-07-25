117129b365700c50a38eccdb16509b9407e8bda1
Stephen Woller <swoller22@gmail.com>
2018-07-24 20:54:13 -0400

    Select an image from gallery

--- mosaic/app/src/main/java/com/example/matthew/mosaic/BaseImageActivity.java
+++ mosaic/app/src/main/java/com/example/matthew/mosaic/BaseImageActivity.java
@@ -1,22 +1,76 @@
 package com.example.matthew.mosaic;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class BaseImageActivity extends AppCompatActivity {
+
+    private Button getImageBtn;
+    private ImageView baseImage;
+    private int GALLERY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_image);
+
+        getImageBtn = (Button)findViewById(R.id.getImageBtn);
+        baseImage = (ImageView)findViewById(R.id.baseImage);
+
+        getImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
     }
 
+    public void chooseImage(){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
+        startActivityForResult(galleryIntent, GALLERY);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    Toast.makeText(BaseImageActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+                    baseImage.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(BaseImageActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
