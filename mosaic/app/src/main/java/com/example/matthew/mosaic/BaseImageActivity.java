ed1f2fffe9ea0239203195d060d27f6cb8a26942
Stephen Woller <swoller22@gmail.com>
2018-08-29 19:48:30 -0400

    playing with bitmaps

--- mosaic/app/src/main/java/com/example/matthew/mosaic/BaseImageActivity.java
+++ mosaic/app/src/main/java/com/example/matthew/mosaic/BaseImageActivity.java
@@ -2,6 +2,7 @@ package com.example.matthew.mosaic;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -12,7 +13,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class BaseImageActivity extends AppCompatActivity {
 
@@ -22,6 +26,9 @@ public class BaseImageActivity extends AppCompatActivity {
     private int IMAGE_SELECTOR = 0;
     private int LITTLE_IMAGES_SELECTOR = 1;
     private Bitmap baseImageBitmap;
+    BitmapFactory.Options bitmapOptions;
+    private int THUMBNAIL_SIZE = 70;
+    ArrayList<RGB> littleImagesRGBArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,7 @@ public class BaseImageActivity extends AppCompatActivity {
         getImageBtn = (Button)findViewById(R.id.getImageBtn);
         getLittleImagesBtn = (Button)findViewById(R.id.getLittleImagesBtn);
         baseImage = (ImageView)findViewById(R.id.baseImage);
+        littleImagesRGBArrayList = new ArrayList<RGB>();
 
         getImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,9 +70,6 @@ public class BaseImageActivity extends AppCompatActivity {
                 try {
                     baseImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
                     baseImage.setImageBitmap(baseImageBitmap);
-                    BitmapExtension bitmapExtension = new BitmapExtension(baseImageBitmap);
-                    RGB averageRGB = bitmapExtension.getAverageRGB();
-
                 } catch (IOException e) {
                     e.printStackTrace();
                     Toast.makeText(BaseImageActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
@@ -76,8 +81,11 @@ public class BaseImageActivity extends AppCompatActivity {
             for(int i = 0; i < count; i++) {
                 Uri imageUri = data.getClipData().getItemAt(i).getUri();
                 try {
-                    Bitmap miniBitmap =  MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                    Log.d("RGBVal", "onActivityResult: "+miniBitmap.getPixel(300,300));
+                    //Bitmap miniBitmap =  MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    Bitmap miniBitmap = getMiniBitmap(imageUri);
+                    BitmapExtension miniBitmapExtension = new BitmapExtension(miniBitmap);
+                    RGB averageRGB = miniBitmapExtension.getAverageRGB();                   //Get the average RGB value for each selected little image
+                    littleImagesRGBArrayList.add(averageRGB);                               //Add the most recent average RGB value to the ArrayList
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -100,29 +108,39 @@ public class BaseImageActivity extends AppCompatActivity {
         startActivityForResult(littleImageSelectorIntent, LITTLE_IMAGES_SELECTOR);
     }
 
-    public int[] getAverageRGBValueOfImage(Bitmap imageBitmap){
-        int baseImageBitmapWidth = imageBitmap.getWidth();
-        int baseImageBitmapHeight = imageBitmap.getHeight();
-        int[] baseImagePixels = new int[baseImageBitmapWidth*baseImageBitmapHeight];
-        int R = 0,G = 0,B = 0;
-        int R_average = 0, G_average = 0, B_average = 0;
-        int cnt = 0;
+    public Bitmap getMiniBitmap(Uri uri) throws FileNotFoundException, IOException{
+        InputStream input = this.getContentResolver().openInputStream(uri);
 
-        for(int y = 0; y < baseImageBitmapHeight; y++) {
-            for(int x = 0; x < baseImageBitmapWidth; x++) {
-                int index = y * baseImageBitmapWidth + x;
-                R = (baseImagePixels[index] >> 16) & 0xff;     //bitwise shifting
-                G = (baseImagePixels[index] >> 8) & 0xff;
-                B = baseImagePixels[index] & 0xff;
+        BitmapFactory.Options onlyBoundsOptions = new BitmapFactory.Options();
+        onlyBoundsOptions.inJustDecodeBounds = true;
+        onlyBoundsOptions.inDither=true;//optional
+        onlyBoundsOptions.inPreferredConfig=Bitmap.Config.ARGB_8888;//optional
+        BitmapFactory.decodeStream(input, null, onlyBoundsOptions);
+        input.close();
 
-                Log.d("Average R, G, B: ", ""+R);
-                R_average = R_average + R;
-                G_average += G;
-                B_average += B;
-                cnt++;
-            }
+        if ((onlyBoundsOptions.outWidth == -1) || (onlyBoundsOptions.outHeight == -1)) {
+            return null;
         }
-        return new int[] {R_average, G_average, B_average};
+
+        int originalSize = (onlyBoundsOptions.outHeight > onlyBoundsOptions.outWidth) ? onlyBoundsOptions.outHeight : onlyBoundsOptions.outWidth;
+
+        double ratio = (originalSize > THUMBNAIL_SIZE) ? (originalSize / THUMBNAIL_SIZE) : 1.0;
+
+        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+        bitmapOptions.inSampleSize = getPowerOfTwoForSampleRatio(ratio);
+        bitmapOptions.inDither = true; //optional
+        //bitmapOptions.inPreferredConfig=Bitmap.Config.ARGB_8888;//
+        input = this.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+        input.close();
+        return bitmap;
     }
+
+    private static int getPowerOfTwoForSampleRatio(double ratio){
+        int k = Integer.highestOneBit((int)Math.floor(ratio));
+        if(k==0) return 1;
+        else return k;
+    }
+
 }
 
--- mosaic/app/src/main/java/com/example/matthew/mosaic/BitmapExtension.java
+++ mosaic/app/src/main/java/com/example/matthew/mosaic/BitmapExtension.java
@@ -1,6 +1,8 @@
 package com.example.matthew.mosaic;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.util.Log;
 
 public class BitmapExtension {
     Bitmap imageBitmap;
@@ -12,11 +14,34 @@ public class BitmapExtension {
 
     public RGB getAverageRGB() {
         // calculate average RGB values
-        int averageR = 0;
-        int averageG = 0;
-        int averageB = 0;
 
-        return new RGB(averageR, averageG, averageB);
+        int baseImageBitmapWidth = imageBitmap.getWidth();
+        int baseImageBitmapHeight = imageBitmap.getHeight();
+        int[] baseImagePixels = new int[baseImageBitmapWidth*baseImageBitmapHeight];
+        imageBitmap.getPixels(baseImagePixels, 0, baseImageBitmapWidth, 0, 0,
+                baseImageBitmapWidth, baseImageBitmapHeight);
+
+        int R = 0,G = 0,B = 0;
+        float averageR = 0, averageG = 0, averageB = 0;
+        int cnt = 0;
+
+        for(int y = 0; y < baseImageBitmapHeight; y++) {
+            for(int x = 0; x < baseImageBitmapWidth; x++) {
+                int index = y * baseImageBitmapWidth + x;
+
+                R = Color.red(baseImagePixels[index]);
+                G = Color.green(baseImagePixels[index]);
+                B = Color.blue(baseImagePixels[index]);
+
+                averageR += R;
+                averageG += G;
+                averageB += B;
+                cnt++;
+            }
+        }
+
+        return new RGB(averageR/cnt, averageG/cnt, averageB/cnt);
+
     }
 
 }
